e7155e503560b0080327fec7820e80b89085e17f
rebecca15sousa <64431799+rebecca15sousa@users.noreply.github.com>
2021-01-05 16:24:14 -0500

    Created on click functions to filter items by their respective lists

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 let inputField = document.getElementById("textInput");
+let allBtn = document.getElementById("allBtn");
+let activeBtn = document.getElementById("activeBtn");
+let completedBtn = document.getElementById("completedBtn");
 
 //adds new item to Active List
 function createItem() {
@@ -13,8 +16,28 @@ function createItem() {
     document.getElementById("textInput").value = "";
 }
 
+//event listener for "Enter" key when typing in input field
 inputField.addEventListener("keyup", function(event) {
     if (event.key === "Enter") {
         createItem();
     }
 });
+
+//functions to show/hide lists
+allBtn.onclick = function() {
+    //document.getElementById("allList").style.display = "block";
+    document.getElementById("activeList").style.display = "block";
+    document.getElementById("completedList").style.display = "block";
+}
+
+activeBtn.onclick = function() {
+    //document.getElementById("allList").style.display = "none";
+    document.getElementById("activeList").style.display = "block";
+    document.getElementById("completedList").style.display = "none";
+}
+
+completedBtn.onclick = function() {
+    //document.getElementById("allList").style.display = "none";
+    document.getElementById("activeList").style.display = "none";
+    document.getElementById("completedList").style.display = "block";
+}
